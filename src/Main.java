ff31407216cc2f19e26e3dd95bbbcb764f57792d
Kirillov Alexander <kirillovbrest@gmail.com>
2020-12-03 12:15:41 +0300

    Added comment

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1,1);
-        int c = calc.divide.apply(a, b);
+        int c = calc.divide.apply(a, b); //Код не работал, т.к. в лямбда-выражении divide не было проверки на знаменатель = 0
 
         int d = calc.abs.apply(-1);
 
